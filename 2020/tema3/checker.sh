c18bb0523d6f49922af5bd4cead8c67c7c83bc1e
Dorinel Filip <dorinelfilip@users.noreply.github.com>
2021-01-07 13:46:40 +0200

    Update

--- 2020/tema3/checker.sh
+++ 2020/tema3/checker.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+export ONLINE_JUDGE=True
+export PYTHONMALLOC=malloc_debug
+
+echo "Version 0.9"
+
 date
 
 unzip -q /usr/lib/pc2020-tema3-checker.zip -d .
 unzip -q archive.zip
+uzip -q artifact.zip
 
-./check
+cat grader.patch | patch -p0
+
+#./check
 
 date
