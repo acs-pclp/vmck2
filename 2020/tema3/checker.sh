e0ee04e83d5d1bdb6797c7340c5d4cf38400bb20
Dorinel Filip <dorinelfilip@users.noreply.github.com>
2021-01-07 23:14:23 +0200

    Update checker.sh

--- 2020/tema3/checker.sh
+++ 2020/tema3/checker.sh
@@ -2,17 +2,12 @@
 
 export ONLINE_JUDGE=True
 
-echo "Version 0.9.2"
+echo "Version 1.0 - Online"
 
 date
 
 unzip -q /usr/lib/pc2020-tema3-checker.zip -d .
 unzip -q archive.zip
-unzip -q artifact.zip
-
-cat grader.patch | patch -p0
-cat config.patch | patch -p0
-
 ./check
 
 date
