b67a7c6056ca9485b6f6a86e472d8e7ea95f02bc
Dorinel Filip <dorinelfilip@users.noreply.github.com>
2021-01-28 22:41:47 +0200

    Fixed

--- 2020/tema3/checker.sh
+++ 2020/tema3/checker.sh
@@ -2,9 +2,13 @@
 
 export ONLINE_JUDGE=True
 
-echo "Version 1.0 - RC Online"
+echo "Version 1.x - RC Online"
 
 date
+
+unzip -q /usr/lib/pc2020-tema3-checker.zip -d .
+unzip -q archive.zip
+unzip -q artifact.zip
 
 mkdir bk
 cp -r tasks/image_editor/tests/00-image_editor bk/00-image_editor
@@ -12,10 +16,6 @@ cp -r tasks/image_editor/tests/00-image_editor bk/01-image_editor
 rm -rf 58-image_editor/tests/*
 cp -r bk/* tasks/image_editor/tests/
 rm -rf bk
-
-unzip -q /usr/lib/pc2020-tema3-checker.zip -d .
-unzip -q archive.zip
-unzip -q artifact.zip
 
 cat config.patch | patch -p0
 cat nume.txt
