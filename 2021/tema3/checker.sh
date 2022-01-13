69f66a55e9e77d4979665c8d332de66668ca3d3a
Dorinel Filip <ion_dorinel.filip@cti.pub.ro>
2022-01-14 00:55:52 +0200

    Update checker.sh

--- 2021/tema3/checker.sh
+++ 2021/tema3/checker.sh
@@ -2,9 +2,11 @@
 
 export ONLINE_JUDGE=True
 
-echo "Version 1.0 - RC Online"
+echo "Version 1.1 - RC Online"
 
 date
+
+sha256sum /usr/lib/pc2021-tema3-checker.zip
 
 unzip -q /usr/lib/pc2021-tema3-checker.zip -d .
 unzip -q archive.zip
