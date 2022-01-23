5171321116279c108579c673dfe87424a77cb7ac
Dorinel Filip <ion_dorinel.filip@cti.pub.ro>
2022-01-23 07:10:25 +0200

    Update checker.sh

--- 2021/tema3/checker.sh
+++ 2021/tema3/checker.sh
@@ -2,7 +2,7 @@
 
 export ONLINE_JUDGE=True
 
-echo "Version 1.3 - Valgrind Only - RC Online"
+echo "Version 1.4 - Valgrind Only - RC Online"
 
 date
 
@@ -23,11 +23,11 @@ cp tasks/image_editor/tests/00-image_editor/00-image_editor.ref bk/00-image_edit
 cp tasks/image_editor/tests/01-image_editor/01-image_editor.in bk/01-image_editor
 cp tasks/image_editor/tests/01-image_editor/01-image_editor.ref bk/01-image_editor
 
-cp tasks/image_editor/tests/05-image_editor/05-image_editor.in bk/02-image_editor
-cp tasks/image_editor/tests/05-image_editor/05-image_editor.ref bk/02-image_editor
+cp tasks/image_editor/tests/05-image_editor/05-image_editor.in bk/02-image_editor/02-image_editor.in
+cp tasks/image_editor/tests/05-image_editor/05-image_editor.ref bk/02-image_editor/02-image_editor.ref
 
-cp tasks/image_editor/tests/55-image_editor/55-image_editor.in bk/03-image_editor
-cp tasks/image_editor/tests/55-image_editor/55-image_editor.ref bk/03-image_editor
+cp tasks/image_editor/tests/55-image_editor/55-image_editor.in bk/03-image_editor/03-image_editor.in
+cp tasks/image_editor/tests/55-image_editor/55-image_editor.ref bk/03-image_editor/03-image_editor.ref
 
 rm -rf tasks/image_editor/tests
 mv bk tasks/image_editor/tests
